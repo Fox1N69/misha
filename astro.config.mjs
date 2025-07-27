fcc1f1cc38835db3f4c70764d80bd76b630b8e7e
Pavel Maksimovich <diogoc1707@gmail.com>
2025-07-27 17:33:10 +0500

    setup SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
+import node from '@astrojs/node';
 
-// https://astro.build/config
 export default defineConfig({
     site: 'https://mikhailkolesnikov.com',
     output: "server",
+    adapter: node({ mode: "standalone" }),
 });
